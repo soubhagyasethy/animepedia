002b49aef9a48742fb50a210633092b6dadbe23d
soubhagyasethy <ssethy017@gmail.com>
2024-01-01 23:57:03 +0530

    added comments

--- src/AnimeCard.js
+++ src/AnimeCard.js
@@ -4,6 +4,7 @@ class AnimeCard extends Component {
   render() {
     return (
       <div className="anime-card">
+        {/* using props to get the data sent from animeList  */}
         <img src={this.props.image} alt={this.props.name} />
         <p>{this.props.name}</p>
       </div>
--- src/AnimeList.js
+++ src/AnimeList.js
@@ -1,12 +1,11 @@
 import { Component } from "react";
 import AnimeCard from "./AnimeCard";
 
-// Complete the AnimeList Component
 class AnimeList extends Component {
   render() {
     return (
       <div className="anime-list">
-        {/* Map the anime list recieved through props and pass the details to the Animecard component*/}
+        {/* sending anime data to anime card one by one using map method and props  */}
         {this.props.anime.map((anime, i) => {
           return <AnimeCard name={anime.name} image={anime.image} key={i} />;
         })}

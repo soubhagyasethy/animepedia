ec3d97fda5c6cc0eb0de752050cfcbadff4e4e0a
soubhagyasethy <ssethy017@gmail.com>
2024-01-01 23:52:57 +0530

    feat: completed Animecard component

--- src/AnimeCard.js
+++ src/AnimeCard.js
@@ -1,12 +1,13 @@
 import { Component } from "react";
 
-// Complete the AnimeCard Component
 class AnimeCard extends Component {
   render() {
-    return <div className="anime-card">
-      <img src={} alt={} />
-        <p>{}</p>
-    </div>;
+    return (
+      <div className="anime-card">
+        <img src={this.props.image} alt={this.props.name} />
+        <p>{this.props.name}</p>
+      </div>
+    );
   }
 }
 
